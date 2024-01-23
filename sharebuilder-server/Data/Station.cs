2363a11d1b450fadf4f01d6d553d735f239a7fc2
Jessica Alin González <jag816@gmail.com>
2024-01-23 14:05:23 -0500

    add files for rendering stations

---
+++ sharebuilder-server/Data/Station.cs
@@ -0,0 +1,10 @@
+﻿namespace sharebuilder_server.Data;
+
+public class Station
+{
+    public required string CallLetters { get; set; }
+
+    public required string Market { get; set; }
+
+    public required string Affiliation { get; set; }
+}
---
+++ sharebuilder-server/Data/StationService.cs
@@ -0,0 +1,18 @@
+﻿namespace sharebuilder_server.Data;
+
+public class StationService
+{
+    private List<Station> Stations = new List<Station>
+    {
+        new Station { CallLetters = "KTLA-TV", Market = "Los Angeles", Affiliation = "CW" },
+        new Station { CallLetters = "KABC-TV", Market = "Los Angeles", Affiliation = "ABC" },
+        new Station { CallLetters = "KCBS-TV", Market = "Los Angeles", Affiliation = "CBS" },
+        new Station { CallLetters = "KNBC", Market = "Los Angeles", Affiliation = "NBC" },
+    };
+
+    public Task<List<Station>> GetStationsAsync()
+    {
+        return Task.FromResult(Stations);
+    }
+}
+
